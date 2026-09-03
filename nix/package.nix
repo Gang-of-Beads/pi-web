9d3bb8f7e13e47678a46a7eb5a4d6eb48a50f2ce
Hanxiao Du <vincent.hanxiao.du@gmail.com>
2026-09-03 19:35:03 +0400

    chore(release): v1.202609.9

--- nix/package.nix
+++ nix/package.nix
@@ -11,7 +11,7 @@ buildNpmPackage rec {
   npmDepsFetcherVersion = 2;
   # Includes package-lock's root package version as well as dependency entries.
   # Update with `nix build .#pi-web --no-link` whenever package-lock.json moves.
-  npmDepsHash = "sha256-Vy8F+oSioLNDFnCueC49Ww2QdS8QCvo60NwdhtZzodI=";
+  npmDepsHash = "sha256-ZaLd+45gNrS/3PZNeSB7mmpKO8feio8qN3c1kqhbqBI=";
 
   nativeBuildInputs = [ makeWrapper python3 pkg-config ]
     ++ lib.optionals stdenv.isLinux [ stdenv.cc ];
